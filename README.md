79b18de357224681dc1b1d1ac9faa55c28ea6c5e
Pascal S. de Kloe <pascal@quies.net>
2023-07-01 14:59:18 +0200

    benchmark with random values

--- README.md
+++ README.md
@@ -8,3 +8,30 @@ This is free and unencumbered software released into the
 
 [![API Documentation](https://godoc.org/github.com/pascaldekloe/pile?status.svg)](https://godoc.org/github.com/pascaldekloe/pile)
 [![Build Status](https://github.com/pascaldekloe/pile/actions/workflows/go.yml/badge.svg)](https://github.com/pascaldekloe/pile/actions/workflows/go.yml)
+
+
+# Performance
+
+The implementation shines when reads or writes operate on a small or predictable
+key range. Without the CPU cache in our favour, operation slows down to a little
+over one million per second.
+
+```
+goos: darwin
+goarch: arm64
+pkg: github.com/pascaldekloe/pile
+BenchmarkMapFind/Sequential/1Ki-8     	136814722	         8.288 ns/op
+BenchmarkMapFind/Sequential/1Mi-8     	39862473	        30.96 ns/op
+BenchmarkMapFind/Sequential/64Mi-8    	35958916	        33.09 ns/op
+BenchmarkMapFind/Random/1Ki-8         	79347367	        13.21 ns/op
+BenchmarkMapFind/Random/1Mi-8         	 3581359	       333.5 ns/op
+BenchmarkMapFind/Random/64Mi-8        	 1595295	       778.6 ns/op
+BenchmarkInsert/Append/map-8          	28757638	        41.31 ns/op
+BenchmarkInsert/Append/set-8          	29841837	        39.63 ns/op
+BenchmarkInsert/Prepend/map-8         	28124312	        46.20 ns/op
+BenchmarkInsert/Prepend/set-8         	29796175	        42.58 ns/op
+BenchmarkInsert/Random/map-8          	 4317328	       493.5 ns/op
+BenchmarkInsert/Random/set-8          	 5090876	       470.5 ns/op
+PASS
+ok  	github.com/pascaldekloe/pile	111.711s
+```
--- bench_test.go
+++ bench_test.go
@@ -1,88 +1,176 @@
 package pile
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func BenchmarkMapFind(b *testing.B) {
-	b.Run("empty", func(b *testing.B) {
-		benchmarkMapFindSeq(b, 0)
+	b.Run("Sequential", func(b *testing.B) {
+		var seq1Ki Map[int, string]
+		for i := 0; i < 1024; i++ {
+			seq1Ki.Put(i, "fill")
+		}
+		var seq1Mi Map[int, string]
+		for i := 0; i < 1024*1024; i++ {
+			seq1Mi.Put(i, "fill")
+		}
+		var seq64Mi Map[int, string]
+		for i := 0; i < 64*1024*1024; i++ {
+			seq64Mi.Put(i, "fill")
+		}
+
+		b.Run("1Ki", func(b *testing.B) {
+			const mask = 1024 - 1
+			for i := 0; i < b.N; i++ {
+				if _, ok := seq1Ki.Find(i & mask); !ok {
+					b.Fatalf("key %d not found", i&mask)
+				}
+			}
+		})
+		b.Run("1Mi", func(b *testing.B) {
+			const mask = 1024*1024 - 1
+			for i := 0; i < b.N; i++ {
+				if _, ok := seq1Mi.Find(i & mask); !ok {
+					b.Fatalf("key %d not found", i&mask)
+				}
+			}
+		})
+		b.Run("64Mi", func(b *testing.B) {
+			const mask = 64*1024*1024 - 1
+			for i := 0; i < b.N; i++ {
+				if _, ok := seq64Mi.Find(i & mask); !ok {
+					b.Fatalf("key %d not found", i&mask)
+				}
+			}
+		})
 	})
-	b.Run("1M-seq", func(b *testing.B) {
-		benchmarkMapFindSeq(b, 1e6)
-	})
-	b.Run("5M-seq", func(b *testing.B) {
-		benchmarkMapFindSeq(b, 5e6)
+
+	b.Run("Random", func(b *testing.B) {
+		feed := nRandomInts(64 * 1024 * 1024)
+		var rnd1Ki Map[int, string]
+		for _, k := range feed[:1024] {
+			rnd1Ki.Put(k, "fill")
+		}
+		var rnd1Mi Map[int, string]
+		for _, k := range feed[:1024*1024] {
+			rnd1Mi.Put(k, "fill")
+		}
+		var rnd64Mi Map[int, string]
+		for _, k := range feed[:64*1024*1024] {
+			rnd64Mi.Put(k, "fill")
+		}
+
+		b.Run("1Ki", func(b *testing.B) {
+			const mask = 1024 - 1
+			for i := 0; i < b.N; i++ {
+				if _, ok := rnd1Ki.Find(feed[i&mask]); !ok {
+					b.Fatalf("key %d not found", i&mask)
+				}
+			}
+		})
+		b.Run("1Mi", func(b *testing.B) {
+			const mask = 1024*1024 - 1
+			for i := 0; i < b.N; i++ {
+				if _, ok := rnd1Mi.Find(feed[i&mask]); !ok {
+					b.Fatalf("key %d not found", i&mask)
+				}
+			}
+		})
+		b.Run("64Mi", func(b *testing.B) {
+			const mask = 64*1024*1024 - 1
+			for i := 0; i < b.N; i++ {
+				if _, ok := rnd64Mi.Find(feed[i&mask]); !ok {
+					b.Fatalf("key %d not found", i&mask)
+				}
+			}
+		})
 	})
 }
 
-func benchmarkMapFindSeq(b *testing.B, fillSize int) {
-	var m Map[int, string]
-	for i := 0; i < fillSize; i++ {
-		m.Put(i, "fill")
-	}
-	if n := m.Size(); n != fillSize {
-		b.Fatalf("fill size is %d, want %d", n, fillSize)
-	}
+func BenchmarkInsert(b *testing.B) {
+	b.Run("Append", func(b *testing.B) {
+		b.Run("map", func(b *testing.B) {
+			var m Map[int, string]
+			for i := 0; i < b.N; i++ {
+				if !m.Insert(i, "foo") {
+					b.Fatalf("insertion %d denied", i)
+				}
+			}
+		})
+		b.Run("set", func(b *testing.B) {
+			var keys Set[int]
+			for i := 0; i < b.N; i++ {
+				if !keys.Insert(i) {
+					b.Fatalf("insertion %d denied", i)
+				}
+			}
+		})
+	})
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		m.Find(i)
-	}
+	b.Run("Prepend", func(b *testing.B) {
+		b.Run("map", func(b *testing.B) {
+			var m Map[int, string]
+			for i := b.N; i > 0; i-- {
+				if !m.Insert(i, "foo") {
+					b.Fatalf("insertion %d denied", i)
+				}
+			}
+		})
+		b.Run("set", func(b *testing.B) {
+			var keys Set[int]
+			for i := b.N; i > 0; i-- {
+				if !keys.Insert(i) {
+					b.Fatalf("insertion %d denied", i)
+				}
+			}
+		})
+	})
+
+	b.Run("Random", func(b *testing.B) {
+		feed := nRandomInts(10e6)
+
+		b.Run("map", func(b *testing.B) {
+			if b.N > len(feed) {
+				b.Fatalf("random set of %d keys not enough for bench N %d", len(feed), b.N)
+			}
+			var m Map[int, string]
+			for _, k := range feed[:b.N] {
+				if !m.Insert(k, "foo") {
+					b.Fatalf("insertion %d denied", k)
+				}
+			}
+		})
+
+		b.Run("set", func(b *testing.B) {
+			if b.N > len(feed) {
+				b.Fatalf("random set of %d keys not enough for bench N %d", len(feed), b.N)
+			}
+			var keys Set[int]
+			for _, k := range feed[:b.N] {
+				if !keys.Insert(k) {
+					b.Fatalf("insertion %d denied", k)
+				}
+			}
+		})
+	})
 }
 
-func BenchmarkMapAppend(b *testing.B) {
-	b.Run("empty", func(b *testing.B) {
-		benchmarkMapAppend(b, 0)
-	})
-	b.Run("1M-seq", func(b *testing.B) {
-		benchmarkMapAppend(b, 1e6)
-	})
-	b.Run("5M-seq", func(b *testing.B) {
-		benchmarkMapAppend(b, 5e6)
-	})
-}
+func nRandomInts(n int) []int {
+	ints := make([]int, n)
+	have := make(map[int]struct{}, n)
 
-func benchmarkMapAppend(b *testing.B, fillSize int) {
-	var m Map[int, string]
-	for i := -fillSize; i < 0; i++ {
-		m.Put(i, "fill")
-	}
-	if n := m.Size(); n != fillSize {
-		b.Fatalf("fill size is %d, want %d", n, fillSize)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		m.Put(i, "foo")
-	}
-}
-
-func BenchmarkSetAppend(b *testing.B) {
-	b.Run("empty", func(b *testing.B) {
-		benchmarkSetAppend(b, 0)
-	})
-	b.Run("1M-seq", func(b *testing.B) {
-		benchmarkSetAppend(b, 1e6)
-	})
-	b.Run("5M-seq", func(b *testing.B) {
-		benchmarkSetAppend(b, 5e6)
-	})
-}
-
-func benchmarkSetAppend(b *testing.B, fillSize int) {
-	var keys Set[int]
-	for i := -fillSize; i < 0; i++ {
-		if !keys.Insert(i) {
-			b.Fatalf("fill insertion %d denied", i)
+	r := rand.NewSource(42)
+	for i := range ints {
+		for {
+			k := int(r.Int63())
+			if _, ok := have[k]; ok {
+				continue // dupe
+			}
+			ints[i] = k
+			have[k] = struct{}{}
+			break
 		}
 	}
-	if n := keys.Size(); n != fillSize {
-		b.Fatalf("fill size is %d, want %d", n, fillSize)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		if !keys.Insert(i) {
-			b.Fatalf("insertion %d denied", i)
-		}
-	}
+	return ints
 }

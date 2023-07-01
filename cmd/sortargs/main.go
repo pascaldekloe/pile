fa8e3a465b34a29b2774709702c558d4357a2c7e
Pascal S. de Kloe <pascal@quies.net>
2023-07-01 20:49:01 +0200

    sortargs(1) dupe handling

--- cmd/sortargs/main.go
+++ cmd/sortargs/main.go
@@ -8,53 +8,65 @@ import (
 )
 
 var (
+	dedupe           bool
 	reverse          bool
 	terminator       string
 	requires, denies []string
 )
 
 func main() {
-	flag.Func("require", "The output is suppressed with exit status 4 when the `string` is not present amongst the input.", func(s string) error {
+	flag.Func("require", "Output is suppressed with exit status 4 when the `string` equals\nno input argument. Multiple -require options can be used in\nconjunction.", func(s string) error {
 		requires = append(requires, s)
 		return nil
 	})
-	flag.Func("deny", "The output is suppressed with exit status 4 when the `string` is present amongst the input.", func(s string) error {
+	flag.Func("deny", "Output is suppressed with exit status 3 when the `string` equals\nan input argument. Multiple -deny options can be used in\nconjunction.", func(s string) error {
 		denies = append(denies, s)
 		return nil
 	})
-	flag.StringVar(&terminator, "terminate", "\n", "End each output with the `string`.")
+	flag.StringVar(&terminator, "terminate", "\n", "Print `string` after each output.")
+	flag.BoolVar(&dedupe, "dedupe", false, "Print duplicated input only once like uniq(1) does.")
 	flag.BoolVar(&reverse, "reverse", false, "Print output in descending order.")
 	flag.Parse()
 
-	var args pile.Set[string]
+	var args pile.Map[string, int]
 
 	// read
 	for _, s := range flag.Args() {
-		args.Insert(s)
+		n, _ := args.Find(s)
+		args.Put(s, n+1)
 	}
 
 	// filter
 	for _, s := range requires {
-		if !args.Find(s) {
+		if _, ok := args.Find(s); !ok {
 			os.Exit(4)
 		}
 	}
 	for _, s := range denies {
-		if args.Find(s) {
-			os.Exit(4)
+		if _, ok := args.Find(s); !ok {
+			os.Exit(3)
 		}
 	}
 
 	// print
 	if reverse {
 		for c, ok := args.Least(); ok; ok = c.Descend() {
-			os.Stdout.WriteString(c.Key())
-			os.Stdout.WriteString(terminator)
+			print(c)
 		}
 	} else {
 		for c, ok := args.Least(); ok; ok = c.Ascend() {
-			os.Stdout.WriteString(c.Key())
-			os.Stdout.WriteString(terminator)
+			print(c)
 		}
+	}
+}
+
+func print(c pile.Cursor[string, int]) {
+	n := 1
+	if !dedupe {
+		n = c.Value()
+	}
+	for ; n > 0; n-- {
+		os.Stdout.WriteString(c.Key())
+		os.Stdout.WriteString(terminator)
 	}
 }
